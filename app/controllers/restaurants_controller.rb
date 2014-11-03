a8b0b8dc0b8ee124a49697dbfc05f83bee557e6f
Zeeshan Rasool <zrasool88@gmail.com>
2014-11-03 15:50:40 +0000

    Testing editing an entry

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,18 @@
+class RestaurantsController < ApplicationController
+	def index
+		@restaurants = Restaurant.all
+	end
+
+	def new
+		@restaurant = Restaurant.new
+	end
+
+	def create
+		@restaurant = Restaurant.create(params[:restaurant].permit(:name))
+		redirect_to '/restaurants'
+	end
+
+	def show
+		@restaurant = Restaurant.find(params[:id])
+	end
+end
---
+++ spec/features/restaurants_feature_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+describe 'restaurants' do
+	context 'no restaurants have been added' do
+		it 'should display a prompt to add a restaurant' do
+			visit '/restaurants'
+			expect(page).to have_content 'No restaurants'
+			expect(page).to have_link 'Add a restaurant'
+		end
+	end
+
+	context 'restaurants have been added' do
+		before do
+			Restaurant.create(name: 'KFC')
+		end
+
+		it 'should display restaurants' do
+			visit '/restaurants'
+			expect(page).to have_content('KFC')
+			expect(page).not_to have_content('No restaurants yet')
+		end
+	end
+
+	context 'viewing restaurants' do
+		before do
+			@kfc = Restaurant.create(name:'KFC')
+		end
+
+		it 'lets a user view a restaurant' do
+			visit '/restaurants'
+			click_link 'KFC'
+			expect(page).to have_content 'KFC'
+			expect(current_path).to eq "/restaurants/#{@kfc.id}"
+		end
+	end
+
+	context 'editing restaurants' do
+		before do
+			Restaurant.create(name:'KFC')
+		end
+
+		it 'lets a user edit a restaurant' do
+			visit '/restaurants'
+			click_link 'Edit KFC'
+			fill_in 'Name', with: 'Kentucky Fried Chicken'
+			click 'Update Restaurant'
+			expect(page).to have_content 'Kentucky Fried Chicken'
+			expect(current_path).to eq '/restaurants'
+		end
+	end
+end
+
+describe 'creating restaurants' do
+	it 'prompts user to fill out a form, then displays the new restaurant' do
+		visit '/restaurants'
+		click_link 'Add a restaurant'
+		fill_in 'Name', with: 'KFC'
+		click_button 'Create Restaurant'
+		expect(page).to have_content 'KFC'
+		expect(current_path).to eq '/restaurants'
+	end
+end
